88630023870a94dd14d7737a566de23d322558fa
Gabriel Tan <58500317+gabztcr@users.noreply.github.com>
2020-06-15 22:55:46 +0800

    Refactor main screen

--- src/views/MainScreen.js
+++ src/views/MainScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-
+import { Color } from "../styles";
 import HomeScreen from "./home/HomeScreen";
 import ChatOverviewScreen from "./chat/ChatOverviewScreen";
 import AddListingScreen from "./post/AddListingScreen";
@@ -11,6 +11,24 @@ import ProfileScreen from "./profile/ProfileScreen";
 
 const Tab = createBottomTabNavigator();
 
+const renderIcons = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    let iconName;
+    iconName =
+      route.name === "Home"
+        ? "home"
+        : route.name === "Chat"
+        ? "chat"
+        : route.name === "Add"
+        ? "library-plus"
+        : route.name === "Activity"
+        ? "bell"
+        : "account";
+
+    return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+  },
+});
+
 function MainScreen(props) {
   const { navigation, isAuthenticated } = props;
   if (!isAuthenticated) {
@@ -18,28 +36,10 @@ function MainScreen(props) {
   }
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName;
-          iconName =
-            route.name === "Home"
-              ? "home"
-              : route.name === "Chat"
-              ? "chat"
-              : route.name === "Add"
-              ? "library-plus"
-              : route.name === "Activity"
-              ? "bell"
-              : "account";
-
-          return (
-            <MaterialCommunityIcons name={iconName} size={size} color={color} />
-          );
-        },
-      })}
+      screenOptions={renderIcons}
       tabBarOptions={{
-        activeTintColor: "#000000",
-        inactiveTintColor: "#d0d0d0",
+        activeTintColor: Color.Palette[1],
+        inactiveTintColor: Color.Palette[6],
       }}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
